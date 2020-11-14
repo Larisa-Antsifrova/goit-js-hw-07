bb9411be9bd446d17e543182cb82d62c0cd8bc5e
Larisa Antsifrova <larisa.antsifrova@gmail.com>
2020-11-14 21:58:22 +0200

    Added solution to the 4th task.

--- js/task-4.js
+++ js/task-4.js
@@ -4,20 +4,24 @@
 // 2. Создай функции increment и decrement для увеличения и уменьшения значения счетчика
 // 3. Добавь слушатели кликов на кнопки, вызовы функций и обновление интерфейса
 
+//Получение ссылок на span counter-а и на кнопки
 const counterRef = document.getElementById("value");
 const incrementBtnRef = document.querySelector('[data-action="increment"]');
 const decrementBtnRef = document.querySelector('[data-action="decrement"]');
 
+// Создание переменной для хранения текущего значения каунтера
 let counterValue = +counterRef.textContent;
 
+// Функция, увеличивающаязначение каунтера
 function increment() {
   return (counterValue += 1);
 }
-
+// Функция, уменьшающая значение каунтера. Если значение каунтера опускается до 0, то значение остаётся 0 и не уходит в минус.
 function decrement() {
   return counterValue < 1 ? (counterValue = 0) : (counterValue -= 1);
 }
 
+//Добавление слушателей события на кнопки
 incrementBtnRef.addEventListener("click", () => {
   increment();
   counterRef.textContent = counterValue;
