7bb0fb6f18a1a7fbb31b23cdb4f5004055775c27
Larisa Antsifrova <larisa.antsifrova@gmail.com>
2020-11-14 23:18:48 +0200

    Added solution to the 6th task.

---
+++ js/task-6.js
@@ -0,0 +1,31 @@
+// Напиши скрипт, который бы при потере фокуса на инпуте,
+// проверял его содержимое на правильное количество символов.
+
+//1. Сколько символов должно быть в инпуте,
+//указывается в его атрибуте data-length.
+
+//2. Если введено подходящее количество, то border инпута
+// становится зеленым, если неправильное - красным.
+
+// Получение ссылок на инпут и значение атрибута "data-length" инпута
+const inputRef = document.getElementById("validation-input");
+const validLength = +inputRef.attributes["data-length"].value;
+
+// Функция для сравнения значения "data-length" инпута и количества символов текущего значения инпута.
+// Добавляет или убирает классы valid/invalid.
+function validateInput() {
+  const currentInputLength = inputRef.value.length;
+
+  if (currentInputLength === validLength) {
+    inputRef.classList.add("valid");
+    inputRef.classList.remove("invalid");
+  }
+
+  if (currentInputLength !== validLength) {
+    inputRef.classList.add("invalid");
+    inputRef.classList.remove("valid");
+  }
+}
+
+// Добавляет слушатель события на инпут
+inputRef.addEventListener("blur", validateInput);
