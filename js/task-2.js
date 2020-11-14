6ac8456c06228663708887be11be2dcc9f5b58e6
Larisa Antsifrova <larisa.antsifrova@gmail.com>
2020-11-14 19:55:10 +0200

    Added solution to the 2nd task.

---
+++ js/task-2.js
@@ -0,0 +1,29 @@
+// Ingredients array
+const ingredients = [
+  "Картошка",
+  "Грибы",
+  "Чеснок",
+  "Помидоры",
+  "Зелень",
+  "Приправы",
+];
+
+// Напиши скрипт, который для каждого элемента массива ingredients
+// создаст отдельный li, после чего вставит все li за одну операцию
+// в список ul.ingredients.
+//Для создания DOM - узлов используй document.createElement().
+
+// Получение ссылки на уже существующий в HTML ul
+const ingredientsList = document.getElementById("ingredients");
+console.log(ingredientsList);
+
+// Создание одного массива с созданными лишками для списка
+let items = [];
+const ingredientsItems = ingredients.map((el) => {
+  const item = document.createElement("li");
+  item.textContent = el;
+  items.push(item);
+});
+
+// Добавление лишек в узел ul
+ingredientsList.append(...items);
